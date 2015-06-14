b7cadbbda471b7f6179e64cd8ebb9532bbf20b87
snowman2 <alan.d.snow@byu.edu>
2015-06-13 19:17:54 -0600

    Improved checking if datasets need to be downloaded.

--- tethysapp/erfp_tool/load_datasets.py
+++ tethysapp/erfp_tool/load_datasets.py
@@ -15,58 +15,68 @@ def download_single_watershed_ecmwf_data(watershed,
     """
     Loads single watersheds ECMWF datasets from data store
     """
-    #get data engine
-    data_store = watershed.data_store
-    if 'ckan' == data_store.data_store_type.code_name:
-        #get dataset managers
-        data_manager = ECMWFRAPIDDatasetManager(data_store.api_endpoint,
-                                                data_store.api_key)    
-        #load current datasets
-        data_manager.download_recent_resource(watershed.ecmwf_data_store_watershed_name, 
-                                              watershed.ecmwf_data_store_subbasin_name,
-                                              ecmwf_rapid_prediction_directory)
-
-    path_to_predicitons = os.path.join(ecmwf_rapid_prediction_directory, 
-                                       watershed.ecmwf_data_store_watershed_name, 
-                                       watershed.ecmwf_data_store_subbasin_name)
-
-    #remove oldest datasets if more than 14 exist
-    try:
-        prediction_directories = sorted(os.listdir(path_to_predicitons), 
-                                        reverse=True)[14:]
-        for prediction_directory in prediction_directories:
-            rmtree(os.path.join(path_to_predicitons, prediction_directory))
-    except OSError:
-        pass
+    if ecmwf_rapid_prediction_directory \
+        and os.path.exists(ecmwf_rapid_prediction_directory) \
+        and watershed.ecmwf_data_store_watershed_name \
+        and watershed.ecmwf_data_store_subbasin_name:
+            
+        #get data engine
+        data_store = watershed.data_store
+        if 'ckan' == data_store.data_store_type.code_name:
+            #get dataset managers
+            data_manager = ECMWFRAPIDDatasetManager(data_store.api_endpoint,
+                                                    data_store.api_key)    
+            #load current datasets
+            data_manager.download_recent_resource(watershed.ecmwf_data_store_watershed_name, 
+                                                  watershed.ecmwf_data_store_subbasin_name,
+                                                  ecmwf_rapid_prediction_directory)
+    
+        path_to_predicitons = os.path.join(ecmwf_rapid_prediction_directory, 
+                                           watershed.ecmwf_data_store_watershed_name, 
+                                           watershed.ecmwf_data_store_subbasin_name)
+        if os.path.exists(path_to_predicitons):
+            #remove oldest datasets if more than 14 exist
+            try:
+                prediction_directories = sorted(os.listdir(path_to_predicitons), 
+                                                reverse=True)[14:]
+                for prediction_directory in prediction_directories:
+                    rmtree(os.path.join(path_to_predicitons, prediction_directory))
+            except OSError:
+                pass
 
 def download_single_watershed_wrf_hydro_data(watershed, 
                                              wrf_hydro_rapid_prediction_directory):
     """
     Loads single watersheds WRF-Hydro datasets from data store
     """
-    #get data engine
-    data_store = watershed.data_store
-    if 'ckan' == data_store.data_store_type.code_name:
-        #get dataset managers
-        data_manager = WRFHydroHRRRDatasetManager(data_store.api_endpoint,
-                                                  data_store.api_key)
-        #load current datasets
-        data_manager.download_recent_resource(watershed.wrf_hydro_data_store_watershed_name, 
-                                              watershed.wrf_hydro_data_store_subbasin_name,
-                                              wrf_hydro_rapid_prediction_directory)
-
-    path_to_predicitons = os.path.join(wrf_hydro_rapid_prediction_directory, 
-                                       watershed.wrf_hydro_data_store_watershed_name, 
-                                       watershed.wrf_hydro_data_store_subbasin_name)
-
-    #remove oldest datasets if more than 24 exist
-    try:
-        prediction_files = sorted(os.listdir(path_to_predicitons), 
-                                        reverse=True)[24:]
-        for prediction_file in prediction_files:
-            rmtree(os.path.join(path_to_predicitons, prediction_file))
-    except OSError:
-        pass
+    if wrf_hydro_rapid_prediction_directory \
+        and os.path.exists(wrf_hydro_rapid_prediction_directory) \
+        and watershed.wrf_hydro_data_store_watershed_name \
+        and watershed.wrf_hydro_data_store_subbasin_name:
+        #get data engine
+        data_store = watershed.data_store
+        if 'ckan' == data_store.data_store_type.code_name:
+            #get dataset managers
+            data_manager = WRFHydroHRRRDatasetManager(data_store.api_endpoint,
+                                                      data_store.api_key)
+            #load current datasets
+            data_manager.download_recent_resource(watershed.wrf_hydro_data_store_watershed_name, 
+                                                  watershed.wrf_hydro_data_store_subbasin_name,
+                                                  wrf_hydro_rapid_prediction_directory)
+    
+        path_to_predicitons = os.path.join(wrf_hydro_rapid_prediction_directory, 
+                                           watershed.wrf_hydro_data_store_watershed_name, 
+                                           watershed.wrf_hydro_data_store_subbasin_name)
+    
+        if os.path.exists(path_to_predicitons):
+            #remove oldest datasets if more than 24 exist
+            try:
+                prediction_files = sorted(os.listdir(path_to_predicitons), 
+                                                reverse=True)[24:]
+                for prediction_file in prediction_files:
+                    rmtree(os.path.join(path_to_predicitons, prediction_file))
+            except OSError:
+                pass
 
 
 def load_datasets():
